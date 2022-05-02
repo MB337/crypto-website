f71049a3359c51595541d216799892fd39340c0d
MB337 <mbmatteobianchi@gmail.com>
2022-05-02 20:42:10 +0200

    Work in progress

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ def redirect_homepage():
 def homepage():
     return render_template("homepage.html")
 
+@app.route("/eth")
+def eth():
+    return render_template("ethereum.html")
+
+@app.route("/btc")
+def btc():
+    return render_template("bitcoin.html")
+
+@app.route("/sol")
+def sol():
+    return render_template("solana.html")
+
+@app.route("/dogecoin")
+def doge():
+    return render_template("dogecoin.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
